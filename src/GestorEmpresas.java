a7a1d066af32cdd8962477cd1981fb4591bdec70
Diogu-Simoes <dbsimoes20@gmail.com>
2022-11-23 22:11:19 +0000

    Add sort empresas in GUI

--- src/GestorEmpresas.java
+++ src/GestorEmpresas.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class GestorEmpresas {
     private ArrayList<Empresa> empresas;
@@ -20,6 +21,45 @@ public class GestorEmpresas {
         Localizacao local = new Localizacao(lat, longi);
         Frutaria empresa = new Frutaria(nome,tipo,local, distrito, mediaAnual, custoEmpregados, numProdutos);
         this.empresas.add(empresa);
+    }
+
+    public void ordenarLista(int opcao){
+        //opcao 1 - ordenar por nome
+        if(opcao==0){
+            empresas.sort((atual, proximo) -> atual.getNome().compareTo(proximo.getNome()));
+        }
+        if(opcao==1){
+            empresas.sort((atual, proximo) -> atual.getNome().compareTo(proximo.getNome()));
+            Collections.reverse(empresas);
+        }
+        if(opcao==2){
+            empresas.sort((atual, proximo) -> atual.getTipo().compareTo(proximo.getTipo()));
+        }
+        if(opcao==3){
+            empresas.sort((atual, proximo) -> atual.getTipo().compareTo(proximo.getTipo()));
+            Collections.reverse(empresas);
+        }
+        if(opcao==4){
+            empresas.sort((atual, proximo) -> atual.getDistrito().compareTo(proximo.getDistrito()));
+        }
+        if(opcao==5){
+            empresas.sort((atual, proximo) -> atual.getDistrito().compareTo(proximo.getDistrito()));
+            Collections.reverse(empresas);
+        }
+        if(opcao==6){
+            empresas.sort((atual, proximo) -> Float.compare(atual.getFaturacaoMedia(), proximo.getFaturacaoMedia()));
+        }
+        if(opcao==7){
+            empresas.sort((atual, proximo) -> Float.compare(atual.getFaturacaoMedia(), proximo.getFaturacaoMedia()));
+            Collections.reverse(empresas);
+        }
+        if(opcao==8){
+            empresas.sort((atual, proximo) -> atual.getNome().compareTo(proximo.getNome()));
+        }
+        if(opcao==9){
+            empresas.sort((atual, proximo) -> atual.getNome().compareTo(proximo.getNome()));
+            Collections.reverse(empresas);
+        }
     }
 
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -12,21 +12,149 @@ import javax.swing.table.*;
 
 public class UserInterface extends JFrame {
     private GestorEmpresas gestor;
+    private GridBagConstraints posicao;
     private JPanel menu, baseDados, filtrar, listar, voltar;
     private JLabel textoTitulo;
     private JComboBox<String> caixaFiltros, caixaOrdem;
     private JTable tabela;
     private DefaultTableModel elementos;
+    private InteracoesCaixa selecElemento;
     private JButton botaoBaseDados, botaoOpcoes, botaoSair, botaoVoltar;
+    private InteracoesBotao premirBotao;
 
     public UserInterface(){
 
         // criar o gestor que vai funcionar através da GUI
         gestor = new GestorEmpresas();
+        // criar o listener para os clicks nos botões
+        premirBotao = new InteracoesBotao();
+        // criar o listener para a caixa com os filtros
+        selecElemento = new InteracoesCaixa();
+        // criar a variável que guarda as definições de layout para cada componente antes de ser adicionado
+        posicao = new GridBagConstraints();
+        //temporario
         gestor.addEmpresa("Alma Shopping","Frutaria", 30, 12, 20, 'N', 10, 5, 45, 'E', "Coimbra", 315321.32f, 139232f, 104);
         gestor.addEmpresa("Coimbra Shopping","Frutaria", 30, 12, 20, 'N', 10, 5, 45, 'E', "Coimbra", 315321.32f, 139232f, 104);
         gestor.addEmpresa("Tia Adelaide","Frutaria", 30, 12, 20, 'N', 10, 5, 45, 'E', "Coimbra", 315321.32f, 139232f, 104);
+        // construir a aparencia da janela tendo em conta o tema que o utilizador escolheu na última vez que usou o programa
+        construirAparencia();
+        //construir o painel do menu
+        construirMenu();
+        // construir o painel para filtrar as empresas com as diferentes opções do enunciado
+        construirFiltrar();
+        // construir o painel com a tabela
+        construirListar();
+        // construir o painel com o botão para voltar ao menu;
+        construirVoltar();
+        // criar o painel base de dados que contém o painel para voltar ao menu e os paineis relacionados com a tabela
+        construirBaseDados();
+        // uma vez que o construtor apenas é chamado quando a frame
+        // é criada pela primeira vez sabemos que podemos mostrar
+        // logo o menu depois de estar tudo construído
+        this.add(menu);
+    }
 
+    private class InteracoesBotao implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent evento) {
+            if(evento.getSource() == botaoBaseDados) {
+                mostrarBaseDados();
+            }
+            if(evento.getSource() == botaoOpcoes){
+                mudarTema();
+            }
+            if(evento.getSource() == botaoVoltar) {
+                mostrarMenu();
+            }
+            if(evento.getSource() == botaoSair) {
+                // confirmar saída e terminar
+                if(JOptionPane.showConfirmDialog(null, "Tem a certeza que pretende sair?", null, JOptionPane.YES_NO_OPTION) == 0){
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+    private class InteracoesCaixa implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent evento) {
+            requestFocusInWindow();
+            if(evento.getSource() == caixaOrdem){
+                Integer caixaSelect = caixaOrdem.getSelectedIndex();
+                gestor.ordenarLista(caixaSelect);
+            }
+            recarregarTabela();
+        }
+    }
+
+    //tentar metar tudo num painel e só botão de voltar separado
+    private void mostrarMenu(){
+        this.remove(baseDados);
+        this.add(menu);
+        this.validate();
+        this.repaint();
+    }
+
+    private void mostrarBaseDados(){
+        this.remove(menu);
+        this.add(baseDados);
+        this.validate();
+        this.repaint();
+    }
+
+    private void recarregarTabela(){
+        Integer caixaSelect = caixaFiltros.getSelectedIndex();
+        ArrayList<Empresa> registo = gestor.getEmpresas();
+        elementos.setRowCount(0);
+        if(caixaSelect==0){
+            for (Empresa empresa : registo){
+                elementos.addRow(new Object[]{empresa.getNome(),empresa.getTipo(),empresa.getDistrito(),empresa.getFaturacaoMedia()});
+            }
+        }
+        if(caixaSelect==1){
+            for (Empresa empresa : registo){
+                if(empresa.getTipo().equals("Restaurante")){
+                    elementos.addRow(new Object[]{empresa.getNome(),empresa.getTipo(),empresa.getDistrito(),empresa.getFaturacaoMedia()});
+                }
+            }
+        }
+        if(caixaSelect==2){
+            for (Empresa empresa : registo){
+                if(empresa.getTipo().equals("Frutaria")){
+                    elementos.addRow(new Object[]{empresa.getNome(),empresa.getTipo(),empresa.getDistrito(),empresa.getFaturacaoMedia()});
+                }
+            }
+        }
+    }
+
+    private void mudarTema(){
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            UIManager.put("Button.select",new Color(118,221,221));
+            Color windowsGrey = new Color(240,240,240);
+            UIManager.put("Button.foreground",new ColorUIResource(Color.BLACK));
+            botaoOpcoes.setForeground(Color.BLACK);
+            botaoSair.setForeground(Color.BLACK);
+            botaoVoltar.setForeground(Color.BLACK);
+            botaoBaseDados.setForeground(Color.BLACK);
+            caixaFiltros.setForeground(Color.BLACK);
+            caixaFiltros.setBackground(windowsGrey);
+            caixaOrdem.setForeground(Color.BLACK);
+            caixaOrdem.setBackground(windowsGrey);
+            tabela.setBackground(Color.WHITE);
+            tabela.setForeground(Color.BLACK);
+            SwingUtilities.updateComponentTreeUI(this);
+            SwingUtilities.updateComponentTreeUI(tabela);
+            SwingUtilities.updateComponentTreeUI(botaoVoltar);
+            SwingUtilities.updateComponentTreeUI(caixaFiltros);
+            SwingUtilities.updateComponentTreeUI(caixaOrdem);
+        }
+        catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Não foi possível alterar o tema, irá permanecer o anterior!",null, JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void construirAparencia(){
         // definir o estilo da janela
         this.setTitle("StarThrive");
         this.setSize(720, 720);
@@ -65,17 +193,20 @@ public class UserInterface extends JFrame {
         UIManager.put("Table.selectionForeground",cinza);
         UIManager.put("Table.focusCellBackground",azul);
         UIManager.put("Table.focusCellForeground",cinza);
-        UIManager.put("Table.font",new Font("Arial", Font.BOLD, 15));
+    }
 
+    private void construirVoltar(){
+        voltar = new JPanel();
+        voltar.setLayout(new GridBagLayout());
+        botaoVoltar = new JButton("◄");
+        posicao.insets = new Insets(0,0,0,50);
+        posicao.gridx = 0;
+        posicao.gridy = 3;
+        botaoVoltar.addActionListener(premirBotao);
+        voltar.add(botaoVoltar, posicao);
+    }
 
-        // criar o listener para os clicks nos botões
-        InteracoesBotao premirBotao = new InteracoesBotao();
-        // criar o listener para a caixa com os filtros
-        InteracoesCaixa selecElemento = new InteracoesCaixa();
-        // criar a variável que guarda as definições de layout para cada componente antes de ser adicionado
-        GridBagConstraints posicao = new GridBagConstraints();
-
-        //construir o painel do menu
+    private void construirMenu(){
         menu = new JPanel();
         menu.setLayout(new GridBagLayout());
         textoTitulo = new JLabel("<html>St<img src=" + getClass().getResource("resources/star.gif").toString() + "></FONT>rThrive</html>");
@@ -99,32 +230,17 @@ public class UserInterface extends JFrame {
         posicao.gridy = 3;
         botaoSair.addActionListener(premirBotao);
         menu.add(botaoSair,posicao);
+    }
 
-        // construir o painel para filtrar as empresas com as diferentes opções do enunciado
-        filtrar = new JPanel();
-        filtrar.setLayout(new GridBagLayout());
-        String[] filtros = {"Todas","Restauração","Mercearias"};
-        caixaFiltros = new JComboBox<String>(filtros);
-        caixaFiltros.setLayout(new GridBagLayout());
-        caixaFiltros.setUI(new BasicComboBoxUI());
-        caixaFiltros.setBorder(new EmptyBorder(5, 15, 5, 15));
-        caixaFiltros.addActionListener(selecElemento);
-        posicao.gridx = 0;
-        posicao.gridy = 0;
-        posicao.insets = new Insets(0,100,40,0);
-        filtrar.add(caixaFiltros,posicao);
-        String[] ordem = {"Nome ↑","Nome ↓","Distrito ↑","Distrito ↓","Despesa anual ↑","Despesa anual ↓","Receita anual ↑","Receita anual ↓","Lucro anual ↑","Lucro anual ↓"};
-        caixaOrdem = new JComboBox<String>(ordem);
-        caixaOrdem.setLayout(new GridBagLayout());
-        caixaOrdem.setUI(new BasicComboBoxUI());
-        caixaOrdem.setBorder(new EmptyBorder(5, 15, 5, 15));
-        caixaOrdem.addActionListener(selecElemento);
-        posicao.gridx = 1;
-        posicao.gridy = 0;
-        posicao.insets = new Insets(0,75,40,0);
-        filtrar.add(caixaOrdem,posicao);
+    private void construirBaseDados(){
+        baseDados = new JPanel();
+        baseDados.setLayout(new BorderLayout());
+        baseDados.add(voltar,BorderLayout.WEST);
+        baseDados.add(filtrar,BorderLayout.NORTH);
+        baseDados.add(listar,BorderLayout.CENTER);
+    }
 
-        // construir o painel com a tabela e a funcionalidade de dar scroll caso fica demasiado grande
+    private void construirListar(){
         listar = new JPanel();
         listar.setLayout(new GridBagLayout());
         // por default criamos a tabela a mostrar todas as empresas na primeira vez que o utlizadar aceder à base de dados
@@ -145,140 +261,30 @@ public class UserInterface extends JFrame {
         posicao.gridx = 1;
         posicao.gridy = 0;
         listar.add(scroller);
+    }
 
-        // construir o painel com o botão para voltar ao menu;
-        voltar = new JPanel();
-        voltar.setLayout(new GridBagLayout());
-        botaoVoltar = new JButton("◄");
-        posicao.insets = new Insets(0,0,0,50);
+    private void construirFiltrar(){
+        filtrar = new JPanel();
+        filtrar.setLayout(new GridBagLayout());
+        String[] filtros = {"Todas","Restauração","Mercearias"};
+        caixaFiltros = new JComboBox<String>(filtros);
+        caixaFiltros.setLayout(new GridBagLayout());
+        caixaFiltros.setUI(new BasicComboBoxUI());
+        caixaFiltros.setBorder(new EmptyBorder(5, 15, 5, 15));
+        caixaFiltros.addActionListener(selecElemento);
         posicao.gridx = 0;
-        posicao.gridy = 3;
-        botaoVoltar.addActionListener(premirBotao);
-        voltar.add(botaoVoltar, posicao);
-
-        // criar o painel base de dados que contém todos os outros que pertecem a essa parte do programa
-        baseDados = new JPanel();
-        baseDados.setLayout(new BorderLayout());
-        baseDados.add(voltar,BorderLayout.WEST);
-        baseDados.add(filtrar,BorderLayout.NORTH);
-        baseDados.add(listar,BorderLayout.CENTER);
-
-        // uma vez que o construtor apenas é chamado quando a frame
-        // é criada pela primeira vez sabemos que podemos mostrar
-        // logo o menu depois de estar tudo construído
-        this.add(menu);
-    }
-
-    private class InteracoesBotao implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent evento) {
-            if(evento.getSource() == botaoBaseDados) {
-                mostrarBaseDados();
-            }
-            if(evento.getSource() == botaoVoltar) {
-                mostrarMenu();
-            }
-            if(evento.getSource() == botaoSair) { // confirmar saída e terminar
-                if(JOptionPane.showConfirmDialog(null, "Tem a certeza que pretende sair?", null, JOptionPane.YES_NO_OPTION) == 0){
-                    System.exit(0);
-                }
-            }
-            if(evento.getSource() == botaoOpcoes){
-                mudarTema();
-            }
-        }
-    }
-
-    private class InteracoesCaixa implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent evento) {
-            requestFocusInWindow();
-            if(evento.getSource() == caixaFiltros){
-                Integer caixaSelect = caixaFiltros.getSelectedIndex();
-                if(caixaSelect==0){
-                    System.out.println("teste0");
-                }
-                if(caixaSelect==1){
-                    System.out.println("teste1");
-                }
-                if(caixaSelect==2){
-                    System.out.println("teste2");
-                }
-            }
-            else{
-                Integer caixaSelect = caixaOrdem.getSelectedIndex();
-                if(caixaSelect==0){
-                    System.out.println("teste0");
-                }
-                if(caixaSelect==1){
-                    System.out.println("teste1");
-                }
-                if(caixaSelect==2){
-                    System.out.println("teste2");
-                }
-                if(caixaSelect==3){
-                    System.out.println("teste3");
-                }
-                if(caixaSelect==4){
-                    System.out.println("teste4");
-                }
-                if(caixaSelect==5){
-                    System.out.println("teste5");
-                }
-                if(caixaSelect==6){
-                    System.out.println("teste6");
-                }
-                if(caixaSelect==7){
-                    System.out.println("teste7");
-                }
-                if(caixaSelect==8){
-                    System.out.println("teste8");
-                }
-                if(caixaSelect==9){
-                    System.out.println("teste9");
-                }
-            }
-        }
-    }
-
-    //tentar metar tudo num painel e só botão de voltar separado
-    private void mostrarMenu(){
-        this.remove(baseDados);
-        this.add(menu);
-        this.validate();
-        this.repaint();
-    }
-
-    private void mostrarBaseDados(){
-        this.remove(menu);
-        this.add(baseDados);
-        this.validate();
-        this.repaint();
-    }
-
-    private void mudarTema(){
-        try{
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        Color windowsGrey = new Color(240,240,240);
-        UIManager.put("Button.foreground",new ColorUIResource(Color.BLACK));
-        botaoOpcoes.setForeground(Color.BLACK);
-        botaoSair.setForeground(Color.BLACK);
-        botaoVoltar.setForeground(Color.BLACK);
-        botaoBaseDados.setForeground(Color.BLACK);
-        caixaFiltros.setForeground(Color.BLACK);
-        caixaFiltros.setBackground(windowsGrey);
-        caixaOrdem.setForeground(Color.BLACK);
-        caixaOrdem.setBackground(windowsGrey);
-        tabela.setBackground(Color.WHITE);
-        tabela.setForeground(Color.BLACK);
-        SwingUtilities.updateComponentTreeUI(this);
-        SwingUtilities.updateComponentTreeUI(tabela);
-        SwingUtilities.updateComponentTreeUI(botaoVoltar);
-        SwingUtilities.updateComponentTreeUI(caixaFiltros);
-        SwingUtilities.updateComponentTreeUI(caixaOrdem);
+        posicao.gridy = 0;
+        posicao.insets = new Insets(0,100,40,0);
+        filtrar.add(caixaFiltros,posicao);
+        String[] ordem = {"Nome ↓","Nome ↑","Distrito ↓","Distrito ↑","Despesa anual ↓","Despesa anual ↑","Receita anual ↓","Receita anual ↑","Lucro anual ↓","Lucro anual ↑"};
+        caixaOrdem = new JComboBox<String>(ordem);
+        caixaOrdem.setLayout(new GridBagLayout());
+        caixaOrdem.setUI(new BasicComboBoxUI());
+        caixaOrdem.setBorder(new EmptyBorder(5, 15, 5, 15));
+        caixaOrdem.addActionListener(selecElemento);
+        posicao.gridx = 1;
+        posicao.gridy = 0;
+        posicao.insets = new Insets(0,75,40,0);
+        filtrar.add(caixaOrdem,posicao);
     }
 }
